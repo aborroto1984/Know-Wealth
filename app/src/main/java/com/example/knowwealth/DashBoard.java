b72d3de63693e1bc0baa92d6d00ec0063cc90c47
Ronald Robbins <RGRobbins@student.fullsail.edu>
2022-05-06 07:39:56 -0600

    Added code to update the date field on dashboard to current date.

--- app/src/main/java/com/example/knowwealth/DashBoard.java
+++ app/src/main/java/com/example/knowwealth/DashBoard.java
@@ -3,6 +3,10 @@ package com.example.knowwealth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class DashBoard extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class DashBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        Calendar calendar = Calendar.getInstance();
+        String curDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+
+        TextView viewDate = findViewById(R.id.view_date);
+        viewDate.setText(curDate);
     }
 }
