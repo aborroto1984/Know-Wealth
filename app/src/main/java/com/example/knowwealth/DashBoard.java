5f9d61b746127311136b841757a5bc676c6be637
Ronald Robbins <RGRobbins@student.fullsail.edu>
2022-05-07 12:08:02 -0600

    Made Icon clickable and navigate to menu activity

--- app/src/main/java/com/example/knowwealth/DashBoard.java
+++ app/src/main/java/com/example/knowwealth/DashBoard.java
@@ -2,7 +2,11 @@ package com.example.knowwealth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DashBoard extends AppCompatActivity {
 
@@ -11,4 +15,9 @@ public class DashBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
     }
+
+    public void Menu(View view) {
+        startActivity(new Intent(DashBoard.this, Menu.class));
+    }
+
 }
