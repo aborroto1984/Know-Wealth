c48a3904e4dc17372592f24ebc8a5a1ecefa269a
Ronald Robbins <RGRobbins@student.fullsail.edu>
2022-05-07 13:20:41 -0600

    Add code to navigate to dashboard on successful login. Note: already in dashboard branch.

--- app/src/main/java/com/example/knowwealth/LoginActivity.java
+++ app/src/main/java/com/example/knowwealth/LoginActivity.java
@@ -58,9 +58,7 @@ public class LoginActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-
-                            Toast.makeText(LoginActivity.this, "Authentication Success." + user.getEmail(),
-                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, DashBoard.class));
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
