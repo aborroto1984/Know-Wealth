0d137aef7f0fd8dd4992a5a8af83bee8c921e808
Ronald Robbins <RGRobbins@student.fullsail.edu>
2022-05-07 14:36:57 -0600

    Fixed warnings in menu activity.

--- app/src/main/java/com/example/knowwealth/Menu.java
+++ app/src/main/java/com/example/knowwealth/Menu.java
@@ -22,50 +22,17 @@ public class Menu extends AppCompatActivity {
         TextView notifyLink = findViewById(R.id.notifyLink);
         TextView signOutLink = findViewById(R.id.signoutLink);
 
-        dashBoardLink.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(Menu.this, DashBoard.class));
-            }
-        });
-        utilitiesLink.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(Menu.this, UtilityProcessing.class));
-            }
-        });
-        subsLInk.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(Menu.this, SubscriptionProcessing.class));
-            }
-        });
-        creditCardLink.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(Menu.this, CreditDatesProcessing.class));
-            }
-        });
-        budgetLink.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(Menu.this, BudgetAdd.class));
-            }
-        });
-        notifyLink.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(Menu.this, NotificationSettings.class));
-            }
-        });
-        signOutLink.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.addCategory(Intent.CATEGORY_HOME);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
+        dashBoardLink.setOnClickListener(v -> startActivity(new Intent(Menu.this, DashBoard.class)));
+        utilitiesLink.setOnClickListener(v -> startActivity(new Intent(Menu.this, UtilityProcessing.class)));
+        subsLInk.setOnClickListener(v -> startActivity(new Intent(Menu.this, SubscriptionProcessing.class)));
+        creditCardLink.setOnClickListener(v -> startActivity(new Intent(Menu.this, CreditDatesProcessing.class)));
+        budgetLink.setOnClickListener(v -> startActivity(new Intent(Menu.this, BudgetAdd.class)));
+        notifyLink.setOnClickListener(v -> startActivity(new Intent(Menu.this, NotificationSettings.class)));
+        signOutLink.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         });
     }
 
