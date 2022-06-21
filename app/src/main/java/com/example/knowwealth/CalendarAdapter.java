a308f77888a722ee9de4b7a1a60efc5374a96b6e
Alfredo Borroto <alborroto@student.fullsail.edu>
2022-06-21 17:35:27 -0400

    Spaced the calendar blocks so the dot shows ok

--- app/src/main/java/com/example/knowwealth/CalendarAdapter.java
+++ app/src/main/java/com/example/knowwealth/CalendarAdapter.java
@@ -27,7 +27,7 @@ public class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder> {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.calendar_blocks, parent, false);
         ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-        layoutParams.height = (int)(parent.getHeight()*0.16);
+        layoutParams.height = (int)(parent.getHeight()*0.165);
 
         return new CalendarViewHolder(view, onItemListener);
     }
